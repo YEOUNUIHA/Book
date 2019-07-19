a334e7c24e77a315e7ffa04f3e975b8bc87a0f6a
gkdus23@hhi.co.kr <gkdus23@hhi.co.kr>
2019-07-19 17:12:53 +0900

    테이블 생성 쿼리 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,19 @@
 create table USER_TB (
-userid VARCHAR(20) NOT NULL,
+userid VARCHAR(20) NOT NULL
+constraint USER_TB_PK primary key ,
 password VARCHAR(40) NOT NULL
+
 );
 
 create table BOOK_TB (
-
+    isbn VARCHAR(30) NOT NULL
+    constraint BOOK_TB_PK primary key,
+title VARCHAR(30),
+image BLOB,
+description VARCHAR(200),
+writer VARCHAR(50),
+publisher VARCHAR(50),
+publicationdate DATE ,
+price VARCHAR(20),
+saleprice VARCHAR(20)
 );
