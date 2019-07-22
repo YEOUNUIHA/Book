016573065b1c24c1e24fd91449b6c0b65059299b
gkdus23@hhi.co.kr <gkdus23@hhi.co.kr>
2019-07-22 17:15:31 +0900

    검색 히스토리 entity생성

--- src/main/java/com/test/book/domain/UserHistory.java
+++ src/main/java/com/test/book/domain/UserHistory.java
@@ -3,13 +3,20 @@ package com.test.book.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 public class UserHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
     private String query;
+    @Column
     private String user;
+    @Column
     private String date;
 }
